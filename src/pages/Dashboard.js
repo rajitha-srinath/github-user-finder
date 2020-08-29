36c0435c49398cff87e530b5a904866e726bfe02
rajitha <rajithasrinath94@gmail.com>
2020-08-29 10:32:38 +0530

    Auth and final

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,12 +5,14 @@ import { GithubContext } from '../context/context';
 
 const Dashboard = () => {
   const { isLoading } = useContext(GithubContext);
-  if(isLoading) {
-    return <main>
-      <Navbar />
-      <Search />
-      <img src={loadingImage} className="loading-img" alt="loading" />
-    </main>
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} className="loading-img" alt="loading" />
+      </main>
+    )
   }
   return (
     <main>
